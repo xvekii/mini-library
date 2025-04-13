ab6bd517050882bb2504cdbef811a742269afe5c
xvekii <vedran.dautanec@gmail.com>
2025-04-13 18:51:31 +0200

    Add replaceChildren(), add code for dynamically creating card-top-icons

--- script.js
+++ script.js
@@ -47,11 +47,98 @@ function addBookToLibrary(title, author, pages, year) {
 }
 
 function displayBooks() {
+  cardContainer.replaceChildren();
+
   myLibrary.forEach((book) => {
     console.log(book.title);
+    
+    // div .card wrapper
     const newCard = document.createElement("div");
     newCard.classList.add("card");
-    newCard.style.display = "block";
+
+    // div .card-top-icons +
+      // span .card-book-icon-span +
+        // image .book-icon +
+      // btn .rm-book-btn +
+        // img .rm-book-icon +
+    
+    // div .card-top-icons
+    const cardTopIconsDiv = document.createElement("div");
+    cardTopIconsDiv.classList.add("card-top-icons");
+    
+    const bookIconSpan = document.createElement("span");
+    bookIconSpan.classList.add("card-book-icon-span");
+
+    const bookIcon = document.createElement("img");
+    bookIcon.classList.add("book-icon");
+    bookIcon.src = "images/book.svg";
+
+    const rmBookBtn = document.createElement("button");
+    rmBookBtn.classList.add("rmBookBtn");
+    
+    const rmBookIcon = document.createElement("img");
+    rmBookIcon.classList.add("rm-book-icon");
+    rmBookIcon.src = "images/remove-book.svg";
+
+
+    // p .card-p
+      // span Title
+      // span .title
+    
+
+    // Appending
+    // card-top-icons
     cardContainer.appendChild(newCard);
+    newCard.appendChild(cardTopIconsDiv);
+
+    cardTopIconsDiv.appendChild(bookIconSpan);
+    bookIconSpan.appendChild(bookIcon);
+
+    cardTopIconsDiv.appendChild(rmBookBtn);
+    rmBookBtn.appendChild(rmBookIcon);
+    
+    // p .card-p
+      // span Title
+      // span .title
+
+
+
+    // p .card-p
+      // span Author
+      // span .author
+
+    // p .card-p
+      // span Pages
+      // span .pages
+
+    // p .card-p
+      // span Book ID
+      // span .ID
+    
+    // p .card-p
+      // span Year
+      // span .year
+
+    // p .card-p
+      // span Read
+      // span .read-status
+
+
+    
+    // const title = document.querySelector(".title");
+    // const author = document.querySelector(".author");
+    // const pages = document.querySelector(".pages");
+    // const ID = document.querySelector(".ID");
+    // const year = document.querySelector(".year");
+    // const read = document.querySelector(".read-status");
+
+    // title.textContent = `${book.title}`; 
+    // author.textContent = `${book.author}`; 
+    // pages.textContent = `${book.pages}`; 
+    // ID.textContent = `${book.ID}`; 
+    // year.textContent = `${book.year}`; 
+    // read.textContent = "No" ;
+    
+    
   });
 }
